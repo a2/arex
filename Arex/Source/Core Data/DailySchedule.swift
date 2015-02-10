9d743be96e1c040a7f8235e3e6e32eb45a5279d4
Alexsander Akers <a2@fb.com>
2015-02-10 21:37:14 +0000

    Remove unnecessary primitive accessors

--- Arex/Source/Core Data/DailySchedule.swift
+++ Arex/Source/Core Data/DailySchedule.swift
@@ -32,42 +32,7 @@ class DailySchedule: Schedule {
     // MARK: - Attributes
 
     @NSManaged var hour: NSNumber?
-    var hourValue: Int16? {
-        get {
-            return hour?.shortValue
-        }
-        set {
-            hour = newValue.map { NSNumber(short: $0) }
-        }
-    }
-
     @NSManaged var minute: NSNumber?
-    var minuteValue: Int16? {
-        get {
-            return minute?.shortValue
-        }
-        set {
-            minute = newValue.map { NSNumber(short: $0) }
-        }
-    }
-
     @NSManaged var repeatCount: NSNumber?
-    var repeatCountValue: Int16? {
-        get {
-            return repeatCount?.shortValue
-        }
-        set {
-            repeatCount = newValue.map { NSNumber(short: $0) }
-        }
-    }
-
     @NSManaged var repeatInterval: NSNumber?
-    var repeatIntervalValue: Int16? {
-        get {
-            return repeatInterval?.shortValue
-        }
-        set {
-            repeatInterval = newValue.map { NSNumber(short: $0) }
-        }
-    }
 }
--- Arex/Source/Core Data/Medication.swift
+++ Arex/Source/Core Data/Medication.swift
@@ -1,4 +1,3 @@
-import AddressBook
 import CoreData
 import UIKit
 
@@ -42,42 +41,20 @@ class Medication: NSManagedObject {
     // MARK: - Attributes
 
     @NSManaged var doctorRecordID: NSNumber?
-    var doctorRecordIDValue: ABRecordID? {
-        get {
-            return doctorRecordID?.intValue
-        }
-        set {
-            doctorRecordID = newValue.map { NSNumber(int: $0) }
-        }
-    }
-
     @NSManaged var dosesLeft: NSNumber?
-    var dosesLeftValue: Int16? {
-        get {
-            return dosesLeft?.shortValue
-        }
-        set {
-            dosesLeft = newValue.map { NSNumber(short: $0) }
-        }
-    }
-
     @NSManaged var lastFilledDate: NSDate?
-
     @NSManaged var name: String?
-
     @NSManaged var note: String?
-
     @NSManaged var pharmacyRecordID: NSNumber?
-    var pharmacyRecordIDValue: ABRecordID? {
-        get {
-            return pharmacyRecordID?.intValue
-        }
-        set {
-            pharmacyRecordID = newValue.map { NSNumber(int: $0) }
-        }
-    }
-
     @NSManaged var pictureData: NSData?
+    @NSManaged var strength: String?
+
+    // MARK: - Relationships
+
+    @NSManaged var schedules: NSOrderedSet
+}
+
+extension Medication {
     var picture: UIImage? {
         get {
             return pictureData.map { UIImage(data: $0) } ?? nil
@@ -86,10 +63,4 @@ class Medication: NSManagedObject {
             pictureData = newValue.map { UIImageJPEGRepresentation($0, 0.7) }
         }
     }
-
-    @NSManaged var strength: String?
-
-    // MARK: - Relationships
-
-    @NSManaged var schedules: NSOrderedSet
 }
