410f208656cc15c99445b3e60df95e1dba315a54
Alexsander Akers <me@a2.io>
2015-06-13 16:41:17 -0700

    Remove non-editing mode from Medication editor

--- Arex/Views/View Controllers/MedicationDetailViewController.swift
+++ Arex/Views/View Controllers/MedicationDetailViewController.swift
@@ -21,164 +21,58 @@ class MedicationDetailViewController: FXFormViewController, MedicationDetailView
 
     private let disposable = CompositeDisposable()
 
-    // MARK: Bar Button Items
-
-    private enum BarButtonItem: CustomStringConvertible {
-        case Done
-        case Edit
-        case Save
-        case Cancel
-
-        var description: String {
-            switch self {
-            case .Done: return "done"
-            case .Edit: return "edit"
-            case .Save: return "save"
-            case .Cancel: return "cancel"
-            }
-        }
-
-        var systemItem: UIBarButtonSystemItem {
-            switch self {
-            case .Done: return .Done
-            case .Edit: return .Edit
-            case .Save: return .Save
-            case .Cancel: return .Cancel
-            }
-        }
-    }
-
-    private func makeBarButtonItem(type: BarButtonItem) -> UIBarButtonItem {
-        precondition(viewModel != nil, "MedicationDetailViewController.viewModel was not assigned before \(type)BarButtonItem loaded")
-
-        let buttonAction: CocoaAction
-        let enabled: PropertyOf<Bool>
-
-        switch type {
-        case .Done:
-            let action: Action<Void, Void, NoError> = Action { _ in
-                self.performSegueWithIdentifier(Constants.SegueIdentifiers.DismissMedicationDetail.rawValue, sender: nil)
-                return .empty
-            }
-
-            buttonAction = CocoaAction(action, void)
-            enabled = PropertyOf(ConstantProperty(true))
-        case .Edit:
-            buttonAction = CocoaAction(viewModel.beginEditing, void)
-            enabled = viewModel.beginEditing.enabled
-        case .Save:
-            buttonAction = CocoaAction(viewModel.saveChanges, void)
-            enabled = viewModel.saveChanges.enabled
-        case .Cancel:
-            buttonAction = CocoaAction(viewModel.revertChanges, void)
-            enabled = viewModel.revertChanges.enabled
-        }
-
-        barButtonItemCocoaActions.insert(buttonAction)
-
-        let barButtonItem = UIBarButtonItem(barButtonSystemItem: type.systemItem, target: buttonAction, action: CocoaAction.selector)
-        enabled.producer.start(next: { [weak barButtonItem] enabled in
-            barButtonItem?.enabled = enabled
-        })
-        return barButtonItem
-    }
-
-    private var barButtonItemCocoaActions = Set<CocoaAction>()
-    private lazy var doneBarButtonItem: UIBarButtonItem = self.makeBarButtonItem(.Done)
-    private lazy var editBarButtonItem: UIBarButtonItem = self.makeBarButtonItem(.Edit)
-    private lazy var saveBarButtonItem: UIBarButtonItem = self.makeBarButtonItem(.Save)
-    private lazy var cancelBarButtonItem: UIBarButtonItem = self.makeBarButtonItem(.Cancel)
-
     deinit {
         disposable.dispose()
+    }
+
+    // MARK: - Actions
+
+    @IBAction private func save(sender: UIBarButtonItem?) {
+        sender?.enabled = false
+        viewModel.saveChanges.apply(())
+            |> start(completed: { [unowned self, weak sender] in
+                sender?.enabled = true
+                self.cancel(sender)
+            })
+    }
+
+    @IBAction private func cancel(sender: UIBarButtonItem?) {
+        performSegueWithIdentifier(Constants.SegueIdentifiers.DismissMedicationDetail.rawValue, sender: sender)
     }
 
     // MARK: - Configuration
 
     private func configureNavigationItem() {
-        let property = viewModel.name.map { (name: String?) -> String in
-            if let name = name where !name.isEmpty {
-                return name
-            } else {
-                return NSLocalizedString("New Medication", comment: "Medication detail view title if medication has empty name")
+        func configureTitle() {
+            let property = viewModel.name.map { (name: String?) -> String in
+                if let name = name where !name.isEmpty {
+                    return name
+                } else {
+                    return NSLocalizedString("New Medication", comment: "Medication detail view title if medication has empty name")
+                }
             }
+
+            disposable += property.producer.start(next: { [unowned self] title in
+                self.navigationItem.title = title
+            })
         }
 
-        disposable += property.producer.start(next: { [unowned self] title in
-            self.navigationItem.title = title
-        })
+        func configureBarButtonItems() {
+            let save = navigationItem.rightBarButtonItem ?? undefined("Navigation item's right bar button item not set")
+            disposable += viewModel.canSave.producer.start(next: { [weak save] canSave in
+                save?.enabled = canSave
+            })
+        }
+
+        configureTitle()
+        configureBarButtonItems()
     }
 
     private func configureTableView() {
         formController.form = viewModel.form
     }
 
-    private func configureEditing() {
-        editing = viewModel.editing.value
-        updateUI(editing, false)
-
-        let beginEditing = viewModel.beginEditing.executing.producer
-            |> skip(1)
-            |> filter(!)
-            |> map(replace(true))
-        let saveChanges = viewModel.saveChanges.executing.producer
-            |> skip(1)
-            |> filter(!)
-            |> map(replace(false))
-        disposable += SignalProducer(values: [beginEditing, saveChanges])
-            |> flatten(.Merge)
-            |> start(next: { [unowned self] editing in
-                self.setEditing(editing, animated: true)
-            })
-        disposable += viewModel.revertChanges.executing.producer
-            |> skip(1)
-            |> start(next: { [unowned self] reverting in
-                if reverting {
-                    self.tableView.beginUpdates()
-                    self.setEditing(false, animated: false)
-                } else {
-                    self.tableView.reloadData()
-                    self.tableView.endUpdates()
-                }
-            })
-    }
-
-    // MARK: - UI Update
-
-    private func updateUI(editing: Bool, _ animated: Bool) {
-        updateNavigationItem(editing, animated)
-    }
-
-    private func updateNavigationItem(editing: Bool, _ animated: Bool) {
-        let rightBarButtonItem, leftBarButtonItem: UIBarButtonItem?
-
-        if editing {
-            rightBarButtonItem = saveBarButtonItem
-            leftBarButtonItem = cancelBarButtonItem
-        } else {
-            rightBarButtonItem = editBarButtonItem
-            leftBarButtonItem = doneBarButtonItem
-        }
-
-        navigationItem.setRightBarButtonItem(rightBarButtonItem, animated: animated)
-        navigationItem.setLeftBarButtonItem(leftBarButtonItem, animated: animated)
-    }
-
     // MARK: - View Life Cycle
-
-    override func setEditing(editing: Bool, animated: Bool) {
-        if !editing && viewModel.isNew {
-            dismissViewControllerAnimated(true, completion: nil)
-            return
-        }
-
-        if !editing {
-            view.endEditing(true)
-        }
-
-        super.setEditing(editing, animated: animated)
-        updateUI(editing, animated)
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -188,15 +82,6 @@ class MedicationDetailViewController: FXFormViewController, MedicationDetailView
 
         configureNavigationItem()
         configureTableView()
-        configureEditing()
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-
-        if !editing && !viewModel.editing.value {
-            navigationItem.rightBarButtonItem = editBarButtonItem
-        }
     }
 
     // MARK: - Form
--- ArexKit/View Models/MedicationDetailViewModel.swift
+++ ArexKit/View Models/MedicationDetailViewModel.swift
@@ -13,8 +13,6 @@ public class MedicationDetailViewModel {
         self.form = MedicationDetailForm(medication: medication)
 
         self.isNew = !get(MedicationLenses.isPersisted, medication)
-        self._editing = MutableProperty(true)
-        self.editing = PropertyOf(self._editing)
 
         let name = DynamicProperty(object: self.form, keyPath: "name")
         self.name = name.map { $0 as? String }
@@ -23,39 +21,11 @@ public class MedicationDetailViewModel {
         self.canSave = self.name.map { name in
             return name.map { !$0.isEmpty } ?? false
         }
-
-        self.beginEditing = Action(enabledIf: self._editing.map(!)) { _ in SignalProducer.empty }
-
-        let revertEnabled = MutableProperty(true)
-        self.revertChanges = Action(enabledIf: revertEnabled) { _ in SignalProducer.empty }
-
-        // All properties have been initialized.
-
-        self._editing.value = self.isNew
-        self._editing.producer
-            |> combineLatestWith(self.saveChanges.executing.producer)
-            |> map { (editing, saving) in editing && !saving }
-            |> start(Event.sink(next: revertEnabled.put))
-
-        self.beginEditing.executing.producer
-            |> filter(boolValue)
-            |> start(next: { [unowned self] _ in
-                self._editing.value = true
-            })
-
-        self.revertChanges.executing.producer
-            |> filter(boolValue)
-            |> start(next: { [unowned self] _ in
-                self.form.medication = self.medication
-                self._editing.value = false
-            })
     }
 
     // MARK: Properties
 
     public let isNew: Bool
-    private let _editing: MutableProperty<Bool>
-    public let editing: PropertyOf<Bool>
     public let canSave: PropertyOf<Bool>
     public let name: PropertyOf<String?>
 
@@ -65,23 +35,15 @@ public class MedicationDetailViewModel {
 
     // MARK: Actions
 
-    public let beginEditing: Action<Void, Void, NoError>
-    public let revertChanges: Action<Void, Void, NoError>
-
-    // This has to be a lazy var (vs let) because it references self.
-    public private(set) lazy var saveChanges: Action<Void, Void, MedicationsControllerError> = {
-        let onNext: Medication -> Void = { [unowned self] in self.medication = $0 }
-        let onComplete: Void -> Void = { [unowned self] in self._editing.value = false }
-
+    public var saveChanges: Action<Void, Void, MedicationsControllerError> {
         return Action(enabledIf: self.canSave) { [unowned self] _ in
             var medication = self.form.medication
             return self.medicationsController.save(medication: &medication)
                 |> map { return undefined("Did not expect MedicationsController.save(_:) to send a next event") }
                 |> concat(SignalProducer { observer, disposable in disposable += SignalProducer(value: medication).start(observer) })
-                |> on(completed: onComplete, next: onNext)
                 |> map(void)
         }
-    }()
+    }
 }
 
 // MARK: - Lenses
