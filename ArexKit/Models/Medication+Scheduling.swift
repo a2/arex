8a04b82c8868e51146fb03e642885072dd5567f5
Alexsander Akers <me@a2.io>
2015-07-24 23:17:36 +0100

    Add Medication scheduling

---
+++ ArexKit/Models/Medication+Scheduling.swift
@@ -0,0 +1,100 @@
+import Pistachio
+import Monocle
+
+private func dayCompare(calendar: NSCalendar)(_ a: NSDate, _ b: NSDate) -> NSComparisonResult {
+    return calendar.compareDate(a, toDate: b, toUnitGranularity: .Day)
+}
+
+private func addDay(calendar: NSCalendar, var date: NSDate) -> NSDate {
+    date = calendar.dateByAddingUnit(.Day, value: 1, toDate: date, options: .MatchNextTime)!
+    date = calendar.startOfDayForDate(date)
+    return date
+}
+
+extension Medication {
+    private func addTimesToDate(calendar: NSCalendar, date: NSDate) -> [NSDate] {
+        let times = get(MedicationLenses.times, self)
+        return times.flatMap { time -> NSDate? in
+            let hour = get(TimeLenses.hour, time)
+            let minute = get(TimeLenses.minute, time)
+            return calendar.dateBySettingHour(hour, minute: minute, second: 0, ofDate: date, options: .MatchNextTimePreservingSmallerUnits)
+        }
+    }
+
+    private func dailyDates(calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate] {
+        return everyXDaysDates(interval: 1, startDate: from)(calendar: calendar, from: from, to: to)
+    }
+
+    private func everyXDaysDates(interval interval: Int, startDate: NSDate)(calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate] {
+        let compare = dayCompare(calendar)
+        if compare(startDate, to) == .OrderedDescending {
+            return []
+        }
+
+        var date = startDate
+        var dates = [NSDate]()
+        var daysSinceFrom = 0
+        while compare(date, to) != .OrderedDescending {
+            if daysSinceFrom % interval == 0 && compare(date, from) != .OrderedAscending {
+                dates += addTimesToDate(calendar, date: date)
+            }
+
+            date = addDay(calendar, date: date)
+            daysSinceFrom++
+        }
+
+        return dates
+    }
+    
+    private func weeklyDates(days days: Int)(calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate] {
+        let compare = dayCompare(calendar)
+        var date = from
+        var dates = [NSDate]()
+        while compare(date, to) != .OrderedDescending {
+            let weekday = calendar.component(.Weekday, fromDate: date)
+            if (days & (1 << (weekday - 1))) != 0 {
+                dates += addTimesToDate(calendar, date: date)
+            }
+
+            date = addDay(calendar, date: date)
+        }
+
+        return dates
+    }
+    
+    private func monthlyDates(days days: Int)(calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate] {
+        let compare = dayCompare(calendar)
+        var date = from
+        var dates = [NSDate]()
+        while compare(date, to) != .OrderedDescending {
+            let monthDay = calendar.component(.Day, fromDate: date)
+            if (days & (1 << (monthDay - 1))) != 0 {
+                dates += addTimesToDate(calendar, date: date)
+            }
+
+            date = addDay(calendar, date: date)
+        }
+
+        return dates
+    }
+    
+    public func dates(inCalendar calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate] {
+        let schedule = get(MedicationLenses.schedule, self)
+        let generator: (calendar: NSCalendar, from: NSDate, to: NSDate) -> [NSDate]
+
+        switch schedule {
+        case .Daily:
+            generator = dailyDates
+        case let .EveryXDays(interval: interval, startDate: startDate):
+            generator = everyXDaysDates(interval: interval, startDate: startDate)
+        case let .Weekly(days: days):
+            generator = weeklyDates(days: days)
+        case let .Monthly(days: days):
+            generator = monthlyDates(days: days)
+        case .NotCurrentlyTaken:
+            generator = { _ in [] }
+        }
+
+        return generator(calendar: calendar, from: from, to: to).sort()
+    }
+}
---
+++ ArexKitTests/MedicationSchedulingTests.swift
@@ -0,0 +1,137 @@
+import ArexKit
+import XCTest
+
+class MedicationSchedulingTests: XCTestCase {
+    var calendar: NSCalendar!
+
+    var date_1JAN2015: NSDate!
+    var date_10JAN2015: NSDate!
+
+    override func setUp() {
+        calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+
+        date_1JAN2015 = calendar.dateFromComponents(dateComponents(1, 1, 2015))!
+        date_10JAN2015 = calendar.dateFromComponents(dateComponents(1, 10, 2015))!
+
+        super.setUp()
+    }
+
+    func time(hour: Int, _ minute: Int) -> Time {
+        return Time(hour: hour, minute: minute)
+    }
+
+    func medication(schedule: Schedule, _ times: Time...) -> Medication {
+        return Medication(schedule: schedule, times: times)
+    }
+
+    func dateComponents(month: Int, _ day: Int, _ year: Int) -> NSDateComponents {
+        let dateComponents = NSDateComponents()
+        dateComponents.month = month
+        dateComponents.day = day
+        dateComponents.year = year
+        return dateComponents
+    }
+
+    func dateComponents(month: Int, _ day: Int, _ year: Int, _ hour: Int, _ minute: Int) -> NSDateComponents {
+        let dateComponents = self.dateComponents(month, day, year)
+        dateComponents.hour = hour
+        dateComponents.minute = minute
+        return dateComponents
+    }
+
+    func date(month: Int, _ day: Int, _ year: Int) -> NSDate {
+        let components = dateComponents(month, day, year)
+        return calendar.dateFromComponents(components)!
+    }
+
+    func date(month: Int, _ day: Int, _ year: Int, _ hour: Int, _ minute: Int) -> NSDate {
+        let components = dateComponents(month, day, year, hour, minute)
+        return calendar.dateFromComponents(components)!
+    }
+
+    func testDailyMedication() {
+        let medication = self.medication(.Daily, time(1, 0), time(2, 0), time(3, 0), time(4, 0))
+        let dates = medication.dates(inCalendar: calendar, from: date_1JAN2015, to: date_10JAN2015)
+
+        let expectedDates = (1...10).flatMap { day in
+            (1...4).map { hour in date(1, day, 2015, hour, 0) }
+        }
+        XCTAssertEqual(dates, expectedDates)
+    }
+
+    func testEveryXDaysMedication() {
+        func test(interval: Int, _ days: [Int]) {
+            let startDate = date(1, 1, 2015, 1, 0)
+
+            let medication = self.medication(.EveryXDays(interval: interval, startDate: startDate), time(1, 0), time(2, 0), time(3, 0), time(4, 0))
+            let dates = medication.dates(inCalendar: calendar, from: date_1JAN2015, to: date_10JAN2015)
+
+            let expectedDates = days.flatMap { day in
+                (1...4).map { hour in date(1, day, 2015, hour, 0) }
+            }
+            XCTAssertEqual(dates, expectedDates)
+        }
+
+        test(1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        test(2, [1, 3, 5, 7, 9])
+        test(3, [1, 4, 7, 10])
+        test(4, [1, 5, 9])
+        test(5, [1, 6])
+    }
+
+    func testWeeklyMedication() {
+        /*
+                January 2015
+            Su Mo Tu We Th Fr Sa
+                         1  2  3
+             4  5  6  7  8  9 10
+            11 12 13 14 15 16 17
+            18 19 20 21 22 23 24
+            25 26 27 28 29 30 31
+        */
+
+        func test(weekDays: Int, days: [Int]) {
+            let medication = self.medication(.Weekly(days: weekDays), time(1, 0), time(2, 0), time(3, 0), time(4, 0))
+            let dates = medication.dates(inCalendar: calendar, from: date_1JAN2015, to: date_10JAN2015)
+
+            let expectedDates = days.flatMap { day in
+                (1...4).map { hour in date(1, day, 2015, hour, 0) }
+            }
+            XCTAssertEqual(dates, expectedDates)
+        }
+
+        test(0b0000000, days: [])
+        test(0b1111111, days: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+        test(0b0000001, days: [4])
+        test(0b0000010, days: [5])
+        test(0b0000100, days: [6])
+        test(0b0001000, days: [7])
+        test(0b0010000, days: [1, 8])
+        test(0b0100000, days: [2, 9])
+        test(0b1000000, days: [3, 10])
+    }
+
+    func testMonthlyMedication() {
+        func test(monthDays: Int, days: [Int]) {
+            let medication = self.medication(.Monthly(days: monthDays), time(1, 0), time(2, 0), time(3, 0), time(4, 0))
+            let dates = medication.dates(inCalendar: calendar, from: date_1JAN2015, to: date_10JAN2015)
+
+            let expectedDates = days.flatMap { day in
+                (1...4).map { hour in date(1, day, 2015, hour, 0) }
+            }
+            XCTAssertEqual(dates, expectedDates)
+        }
+
+        test(0b0000000_0000000_0000000_0000000, days: [])
+        test(0b0000000_0000000_0000000_1111111, days: [1, 2, 3, 4, 5, 6, 7])
+        test(0b1111111_1111111_1111000_0000000, days: [])
+        test(0b1010101_0101010_1010101_0101010, days: [2, 4, 6, 8, 10])
+        test(0b0101010_1010101_0101010_1010101, days: [1, 3, 5, 7, 9])
+    }
+
+    func testNotCurrentTakenMedication() {
+        let medication = self.medication(.NotCurrentlyTaken)
+        let dates = medication.dates(inCalendar: calendar, from: date_1JAN2015, to: date_10JAN2015)
+        XCTAssertEqual(dates, [])
+    }
+}
